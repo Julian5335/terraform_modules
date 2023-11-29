605db65986bc2b542f2f27398179843378538572
Julian5335 <julian2fernandes@gmail.com>
2023-11-30 01:10:11 +0530

    Added cors config

--- http_api_gateway/api_gateway.tf
+++ http_api_gateway/api_gateway.tf
@@ -1,6 +1,12 @@
 resource "aws_apigatewayv2_api" "this" {
   name          = "${var.name}__http_api"
   protocol_type = "HTTP"
+  cors_configuration {
+    allow_origins = var.cors_origins
+    allow_methods = var.cors_methods
+    allow_headers = var.cors_headers
+    max_age = var.max_age
+  }
 }
 
 resource "aws_apigatewayv2_stage" "this" {
--- http_api_gateway/variables.tf
+++ http_api_gateway/variables.tf
@@ -2,3 +2,23 @@ variable "name" {
   type = string
   description = "Name of the api"
 }
+
+variable "cors_origins" {
+  type = list(string)
+  default = [ "*" ]
+}
+
+variable "cors_methods" {
+  type = list(string)
+  default = [ "OPTIONS", "GET", "POST", "PUT", "DELETE" ]
+}
+
+variable "cors_headers" {
+  type = list(string)
+  default = [ "content-type" ]
+}
+
+variable "max_age" {
+  type = number
+  default = 60
+}
