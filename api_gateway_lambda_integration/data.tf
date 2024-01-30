be25ac6a1d788c864399aed3565e61be27d73c83
Julian5335 <julian2fernandes@gmail.com>
2024-01-30 23:09:42 +0530

    Fixed zip file name

--- api_gateway_lambda_integration/data.tf
+++ api_gateway_lambda_integration/data.tf
@@ -1,5 +1,5 @@
 data "archive_file" "this" {
   type        = "zip"
   output_path = "${path.root}/files/${var.function_name}.zip"
-  source_file = "${path.root}/../backend/dist/pull-translation-function.js"
+  source_file = "${path.root}/../backend/dist/${var.function_name}/index.js"
 }
